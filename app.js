655ffcf7c31f1c2e76bc03e46d9d16993bf31773
baderbintahir <badarbintahir@gmail.com>
2020-08-23 19:35:02 +0500

    takes location from command line

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Lahore', (error, data) => {
+const address = process.argv[2]
+
+geocode(address, (error, {latitude, longitude, location} = {}) => {
     if (error) {
         return console.log(error)
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log(error)
         }
 
-        console.log(data.location)
+        console.log(location)
         console.log(forecastData)
     })
 })
-
-
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const weatherstackURL = `http://api.weatherstack.com/current?access_key=1622ff09545de1b5e603175f140f2013&query=${latitude},${longitude}-122.4233&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=1622ff09545de1b5e603175f140f2013&query=${latitude},${longitude}-122.4233&units=f`
 
-    request({ url: weatherstackURL, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the Weather Stack API.', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degress out. It feels like ${response.body.current.feelslike} degrees out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out`)
         }
     })
 }
