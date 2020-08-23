6e3f7acf12ffda1c564b1046dd5efb0aa5de5ffb
baderbintahir <badarbintahir@gmail.com>
2020-08-23 18:23:32 +0500

    get weather of a static location

---
+++ utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYmFkZXJiaW50YWhpciIsImEiOiJja2U2eHV2bWUwNXYxMnlwaTd5cG1rbGo1In0.JJAweWVuYy_Z-uVvOniFWQ&limit=1'
+    request({ url: geocodeURL, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try searching another one.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
